a8471d77cddc569c4d413d1c5a3d156fac464280
danielstumpf <danielstumpf87@gmx.de>
2016-04-26 14:42:02 +0200

    Test Class Rental

--- src/testing/RegularPriceTest.java
+++ src/testing/RegularPriceTest.java
@@ -13,7 +13,7 @@ public class RegularPriceTest {
 		RegularPrice regularPrice = new RegularPrice();
 		int getRegularPrice = regularPrice.getPriceCode();
 		
-		assertThat("Should return th value 0", getRegularPrice, is(0));
+		assertThat("Should return the value 0", getRegularPrice, is(0));
 	}
 
 	@Test
@@ -21,7 +21,7 @@ public class RegularPriceTest {
 		RegularPrice RegularPrice = new RegularPrice();
 		double getRegularPrice = RegularPrice.getCharge(1);
 		
-		assertThat("Should return th value 2", getRegularPrice, is(2.0));
+		assertThat("Should return the value 2", getRegularPrice, is(2.0));
 	}
 
 	@Test
@@ -29,16 +29,14 @@ public class RegularPriceTest {
 		RegularPrice RegularPrice = new RegularPrice();
 		double getRegularPrice = RegularPrice.getCharge(4);
 		
-		assertThat("Should return th value 3", getRegularPrice, is(3.0));
+		assertThat("Should return the value 3", getRegularPrice, is(3.0));
 	}
-	
 	
 	@Test
 	public void testGetFrequentRenterPoints() {
 		RegularPrice RegularPrice = new RegularPrice();
 		int getRegularPrice = RegularPrice.getFrequentRenterPoints(7);
 		
-		assertThat("Should return th value 1", getRegularPrice, is(1));
+		assertThat("Should return the value 1", getRegularPrice, is(1));
 	}
-
 }
--- src/testing/RentalTest.java
+++ src/testing/RentalTest.java
@@ -3,32 +3,59 @@ package testing;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import program.Movie;
+import program.Rental;
 
 public class RentalTest {
 
 	@Test
 	public void testRental() {
-		fail("Not yet implemented");
+		Movie movieInput = new Movie("gnilkreb", 2); 
+		Rental rental = new Rental(movieInput, 3);
+		int daysRented = rental.getDaysRented();
+		assertThat("Should return 3", daysRented, is(3));
+		
+		Movie movie = rental.getMovie();
+		assertThat("Should return gnilkreb", movie, is(movieInput));
 	}
 
 	@Test
 	public void testGetDaysRented() {
-		fail("Not yet implemented");
+		Movie movieInput = new Movie("gnilkreb", 2); 
+		Rental rental = new Rental(movieInput, 3);
+		int daysRented = rental.getDaysRented();
+		assertThat("Should return 3", daysRented, is(3));
+		
 	}
 
 	@Test
 	public void testGetMovie() {
-		fail("Not yet implemented");
+		Movie movieInput = new Movie("gnilkreb", 2); 
+		Rental rental = new Rental(movieInput, 3);
+				
+		Movie movie = rental.getMovie();
+		assertThat("Should return gnilkreb", movie, is(movieInput));
+	
 	}
 
 	@Test
 	public void testGetCharge() {
-		fail("Not yet implemented");
+		Movie movieInput = new Movie("gnilkreb", 2); 
+		Rental rental = new Rental(movieInput, 3);
+		double daysRented = rental.getCharge();
+		assertThat("Should return 1.5", daysRented, is(1.5));
+		
+	
 	}
 
 	@Test
 	public void testGetFrequentRenterPoints() {
-		fail("Not yet implemented");
+		Movie movieInput = new Movie("gnilkreb", 2); 
+		Rental rental = new Rental(movieInput, 3);
+		int daysRented = rental.getFrequentRenterPoints();
+		assertThat("Should return 1", daysRented, is(1));
+		
 	}
 
 }
