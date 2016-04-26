9aa60e9e673fb5d9cba34a7f9a8a65f5b584b170
danielstumpf <danielstumpf87@gmx.de>
2016-04-26 14:39:39 +0200

    Test Class RegularPrice

--- src/testing/RegularPriceTest.java
+++ src/testing/RegularPriceTest.java
@@ -1,24 +1,44 @@
 package testing;
 
 import static org.junit.Assert.*;
-
+import static org.hamcrest.CoreMatchers.is;
 import org.junit.Test;
+
+import program.RegularPrice;
 
 public class RegularPriceTest {
 
 	@Test
 	public void testGetPriceCode() {
-		fail("Not yet implemented");
+		RegularPrice regularPrice = new RegularPrice();
+		int getRegularPrice = regularPrice.getPriceCode();
+		
+		assertThat("Should return th value 0", getRegularPrice, is(0));
 	}
 
 	@Test
-	public void testGetCharge() {
-		fail("Not yet implemented");
+	public void testGetCharge_mitEins() {
+		RegularPrice RegularPrice = new RegularPrice();
+		double getRegularPrice = RegularPrice.getCharge(1);
+		
+		assertThat("Should return th value 2", getRegularPrice, is(2.0));
 	}
 
+	@Test
+	public void testGetCharge_mitVier() {
+		RegularPrice RegularPrice = new RegularPrice();
+		double getRegularPrice = RegularPrice.getCharge(4);
+		
+		assertThat("Should return th value 3", getRegularPrice, is(3.0));
+	}
+	
+	
 	@Test
 	public void testGetFrequentRenterPoints() {
-		fail("Not yet implemented");
+		RegularPrice RegularPrice = new RegularPrice();
+		int getRegularPrice = RegularPrice.getFrequentRenterPoints(7);
+		
+		assertThat("Should return th value 1", getRegularPrice, is(1));
 	}
 
 }
