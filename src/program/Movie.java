41937f9c3108c5cba09a2a8feb778c47aa0ce427
danielstumpf <danielstumpf87@gmx.de>
2016-04-27 13:59:29 +0200

    replaced switch by if-condition

--- src/program/Movie.java
+++ src/program/Movie.java
@@ -1,45 +1,42 @@
 package program;
+
 public class Movie {
-    public static final int CHILDRENS = 2;
-    public static final int REGULAR = 0;
-    public static final int NEW_RELEASE = 1;
-    private String title;
-    private Price price;
+	public static final int CHILDRENS = 2;
+	public static final int REGULAR = 0;
+	public static final int NEW_RELEASE = 1;
+	private String title;
+	private Price price;
 
-    public Movie(String newtitle, int priceCode) {
-        title = newtitle;
-        setPriceCode(priceCode);
-    }
+	public Movie(String newtitle, int priceCode) {
+		title = newtitle;
+		setPriceCode(priceCode);
+	}
 
-    public int getPriceCode() {
-        return price.getPriceCode();
-    }
+	public int getPriceCode() {
+		return price.getPriceCode();
+	}
 
-    public void setPriceCode(int arg) {
-        switch (arg) {
-            case REGULAR:
-                price = new RegularPrice();
-                break;
-            case CHILDRENS:
-                price = new ChildrensPrice();
-                break;
-            case NEW_RELEASE:
-                price = new NewReleasePrice();
-                break;
-            default:
-                throw new IllegalArgumentException("Incorrect Price Code");
-        }
-    }
+	public void setPriceCode(int priceCode) {
+		if (REGULAR == priceCode) {
+			price = new RegularPrice();
+		} else if (CHILDRENS == priceCode) {
+			price = new ChildrensPrice();
+		} else if (NEW_RELEASE == priceCode) {
+			price = new NewReleasePrice();
+		} else {
+			throw new IllegalArgumentException("Incorrect Price Code");
+		}
+	}
 
-    public String getTitle (){
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public double getAmount(int daysRented) {
-        return price.getAmount(daysRented);
-    }
+	public double getAmount(int daysRented) {
+		return price.getAmount(daysRented);
+	}
 
-    public int getFrequentRenterPoints( int daysRented) {
-        return price.getFrequentRenterPoints(daysRented);
-    }
+	public int getFrequentRenterPoints(int daysRented) {
+		return price.getFrequentRenterPoints(daysRented);
+	}
 }
