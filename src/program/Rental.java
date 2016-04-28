360a1b49f6268caad984841107b815f2ebd29113
danielstumpf <danielstumpf87@gmx.de>
2016-04-28 14:27:35 +0200

    use getter for daysRented

--- src/program/Rental.java
+++ src/program/Rental.java
@@ -22,6 +22,6 @@ public class Rental {
 	}
 
 	public int getFrequentRenterPoints() {
-		return getMovie().getFrequentRenterPoints(daysRented);
+		return getMovie().getFrequentRenterPoints(getDaysRented());
 	}
 }
