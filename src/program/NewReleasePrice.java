1ba3709614e339e73592b58127be2d8d4ccb6f9c
danielstumpf <danielstumpf87@gmx.de>
2016-04-28 15:04:21 +0200

    create an overriding method for new releases

--- src/program/NewReleasePrice.java
+++ src/program/NewReleasePrice.java
@@ -11,4 +11,9 @@ public class NewReleasePrice extends Price {
 	double getCharge(int daysRented) {
 		return daysRented * 3;
 	}
+
+	@Override
+	public int getFrequentRenterPoints(int daysRented) {
+		return (daysRented > 1) ? 2 : 1;
+	}
 }
