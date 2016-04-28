7b91674251321f2d53596ed148ff3fd1413aba01
danielstumpf <danielstumpf87@gmx.de>
2016-04-28 13:31:41 +0200

    created htmlStatement method

--- src/program/Customer.java
+++ src/program/Customer.java
@@ -19,6 +19,21 @@ public class Customer {
 		return name;
 	};
 
+	public String htmlStatement() {
+		Enumeration rental = rentals.elements();
+		String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+		while (rental.hasMoreElements()) {
+			Rental each = (Rental) rental.nextElement();
+			// show figures for each rental
+			result += each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+		}
+		// add footer lines
+		result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+		result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints())
+				+ "</EM> frequent renter points<P>";
+		return result;
+	}
+
 	public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
