8fd49041237ebac516e40effed386bc7fff35839
danielstumpf <danielstumpf87@gmx.de>
2016-04-28 15:07:09 +0200

    replaced String with StringBuilder

--- src/program/Program.java
+++ src/program/Program.java
@@ -14,7 +14,8 @@ public class Program {
 	 */
 
 	public static void main(String args[]) {
-		String result;
+		StringBuilder builder = new StringBuilder();
+		
 		System.out.println("Welcome to the Movie Store");
 		Movie m1 = new Movie("movie1", 1);
 		Movie m2 = new Movie("movie2", 2);
@@ -24,7 +25,7 @@ public class Program {
 		c1.addRental(r1);
 		c1.addRental(r2);
 		System.out.println("Let's get the Statement");
-		result = c1.statement();
-		System.out.println(result);
+		builder.append(c1.statement());
+		System.out.println(builder.toString());
 	}
 }
