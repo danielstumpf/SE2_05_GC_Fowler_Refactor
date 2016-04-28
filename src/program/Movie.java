8e9f483e2812b180ceda2a11eb41cc7871b54b60
danielstumpf <danielstumpf87@gmx.de>
2016-04-28 14:51:06 +0200

    Self Encapsulate Field

--- src/program/Movie.java
+++ src/program/Movie.java
@@ -6,6 +6,7 @@ public class Movie {
 	public static final int NEW_RELEASE = 1;
 	private String title;
 	private int priceCode;
+	private Price price;
 
 	public Movie(String newtitle, int priceCode) {
 		title = newtitle;
@@ -13,17 +14,30 @@ public class Movie {
 	}
 
 	public int getPriceCode() {
-		return priceCode;
+		return price.getPriceCode();
 	}
 
 	public void setPriceCode(int arg) {
-		priceCode = arg;
+		switch (arg) {
+		case REGULAR:
+			price = new RegularPrice();
+			break;
+		case CHILDRENS:
+			price = new ChildrensPrice();
+			break;
+		case NEW_RELEASE:
+			price = new NewReleasePrice();
+			break;
+		default:
+			throw new IllegalArgumentException("Incorrect Price Code");
+		}
+
 	}
 
 	public String getTitle() {
 		return title;
 	}
-	
+
 	public double getCharge(int daysRented) {
 		double result = 0;
 		switch (getPriceCode()) {
