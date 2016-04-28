a3ac93b3740ff2dc835653ec7d467eb1e15ff517
danielstumpf <danielstumpf87@gmx.de>
2016-04-28 15:33:22 +0200

    replaced String with StringBuilder replaced Vector with ArrayList removed unused variables

--- src/program/Customer.java
+++ src/program/Customer.java
@@ -8,47 +8,77 @@ public class Customer {
 
 	public Customer(String newname) {
 		name = newname;
-	};
+	}
 
 	public void addRental(Rental arg) {
 		rentals.add(arg);
-	};
+	}
 
 	public String getName() {
 		return name;
-	};
+	}
 
 	public String htmlStatement() {
-		String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+		StringBuilder builder = new StringBuilder();
+		builder.append("<H1>Rentals for <EM>");
+		builder.append(getName());
+		builder.append("</EM></H1><P>\n");
+
 		for (Rental rental : rentals) {
 			// show figures for each rental
-			result += rental.getMovie().getTitle() + ": " + String.valueOf(rental.getCharge()) + "<BR>\n";
+			builder.append(rental.getMovie().getTitle());
+			builder.append(": ");
+			builder.append(String.valueOf(rental.getCharge()));
+			builder.append("<BR>\n");
 		}
 		// add footer lines
-		result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
-		result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints())
-				+ "</EM> frequent renter points<P>";
-		return result;
+		builder.append("<P>You owe <EM>");
+		builder.append(String.valueOf(getTotalCharge()));
+		builder.append("</EM><P>\n");
+		builder.append("On this rental you earned <EM>");
+		builder.append(String.valueOf(getTotalFrequentRenterPoints()));
+		builder.append("</EM> frequent renter points<P>");
+		return builder.toString();
 	}
 
 	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		
-		String result = "Rental Record for " + this.getName() + "\n";
-		result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
+		StringBuilder builder = new StringBuilder();
+
+		builder.append("Rental Record for ");
+		builder.append(this.getName());
+		builder.append("\n");
+		builder.append("\t");
+		builder.append("Title");
+		builder.append("\t");
+		builder.append("\t");
+		builder.append("Days");
+		builder.append("\t");
+		builder.append("Amount");
+		builder.append("\n");
 		for (Rental rental : rentals) {
-			// determine amounts for each line
-			frequentRenterPoints += rental.getFrequentRenterPoints();
+
 			// show figures for this rental
-			result += "\t" + rental.getMovie().getTitle() + "\t" + "\t" + rental.getDaysRented() + "\t"
-					+ String.valueOf(rental.getCharge()) + "\n";
-			totalAmount += rental.getCharge();
+			builder.append("\t");
+			builder.append(rental.getMovie().getTitle());
+			builder.append("\t");
+			builder.append("\t");
+			builder.append(rental.getDaysRented());
+			builder.append("\t");
+			builder.append(String.valueOf(rental.getCharge()));
+			builder.append("\n");
 		}
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-		result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
-		return result;
+		builder.append("Amount owed is ");
+		builder.append(String.valueOf(getTotalCharge()));
+		builder.append("\n");
+		builder.append("You earned ");
+		builder.append(String.valueOf(getTotalFrequentRenterPoints()));
+		builder.append(" frequent renter points");
+		return builder.toString();
+	}
+
+	public double amountFor(Rental aRental) {
+		return aRental.getCharge();
 	}
 
 	private int getTotalFrequentRenterPoints() {
@@ -58,11 +88,7 @@ public class Customer {
 		}
 		return result;
 	}
-
-	public double amountFor(Rental aRental) {
-		return aRental.getCharge();
-	}
-
+	
 	private double getTotalCharge() {
 		double result = 0;
 		for (Rental rental : rentals) {
